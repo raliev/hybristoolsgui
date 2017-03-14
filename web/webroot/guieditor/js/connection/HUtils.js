7e0411bb0dad6ed230e217e71e4262da1df17fda
Artsiom Yemelyanenka <Artsiom_Yemelyanenka@epam.com>
2017-03-14 13:58:43 +0300

    switch to bower for building

--- web/webroot/guieditor/js/connection/HUtils.js
+++ web/webroot/guieditor/js/connection/HUtils.js
@@ -1,5 +1,8 @@
 class HUtils {
     static getTypeCode(pk) {
+        if (("" + pk).length < 9) {
+            return -1;
+        }
         let l = parseInt(pk, 10);
         if (! l) {
             return -1;
